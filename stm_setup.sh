15352d72688e3f854aa99b2c2f49402cc1b07dd8
key <key@devbian>
2020-09-26 17:33:40 +0200

    some add. features

--- stm_setup.sh
+++ stm_setup.sh
@@ -2,8 +2,9 @@
 
 packageManger="apt-get"
 
-
-echo "\nWelcome to STM32 Linux setup\n"
+echo
+echo "Welcome to STM32 Linux setup"
+echo
 
 read -p "do you want to continue (y/n) ?: " yesorno
 if [ "$yesorno" = "y" ];
@@ -11,15 +12,24 @@ then
 	sudo apt install -y gcc-arm-none-eabi
 	sudo apt install -y stlink-tools
 	sudo apt install -y gdb
+	echo
+	echo
+	echo
+	echo ">>> An smtcube/ directory will be created"
 	mkdir stmcube
-	echo "you will be redirected to the download site of ST please donwload the file and place it on the stmcube directory" 
+	echo ">>> you will be redirected to the download site of ST please download the ZIP file and place it on the stmcube/ directory"
 	sensible-browser https://my.st.com/content/my_st_com/en/products/development-tools/software-development-tools/stm32-software-development-tools/stm32-configurators-and-code-generators/stm32cubemx.license=1601130472038.product=STM32CubeMX.version=6.0.1.html
-	sudo unzip stmcube/*.zip
-	sudo chmod +x stmcube/SetupSTM32CubeMX-6.0.1.linux
-	./stmcube/SetupSTM32CubeMX-6.0.1.linux
+	
 
-	echo "do you want to perform st-util --probe ?"
-	read -p 
+	echo
+	echo
+	read -p "Is the download complete and the zip file placed on stmcube/ directory (y/n) ?: " yesorno
+	if [ "$yesorno" = "y" ]; then
+		sudo unzip stmcube/*.zip
+		sudo chmod +x stmcube/SetupSTM32CubeMX-6.0.1.linux
+		./stmcube/SetupSTM32CubeMX-6.0.1.linux
+	fi
+	echo
 
 	read -p "do you want to perform st-info --probe (y/n) ?: " yesorno
 	if [ "$yesorno" = "y" ]; then
