cb6faca7b32102c02b855325045aed6cf212a1b6
key <key@devbian>
2020-09-26 17:21:01 +0200

    reditrection to the download site

--- stm_setup.sh
+++ stm_setup.sh
@@ -2,6 +2,7 @@
 
 packageManger="apt-get"
 
+
 echo "\nWelcome to STM32 Linux setup\n"
 
 read -p "do you want to continue (y/n) ?: " yesorno
@@ -10,9 +11,10 @@ then
 	sudo apt install -y gcc-arm-none-eabi
 	sudo apt install -y stlink-tools
 	sudo apt install -y gdb
-	cd test 
-	make clean 
-	make
+	mkdir stmcube
+	echo "you will be redirected to the download site of ST please donwload the file and place it on the stmcube directory" 
+	sensible-browser https://my.st.com/content/my_st_com/en/products/development-tools/software-development-tools/stm32-software-development-tools/stm32-configurators-and-code-generators/stm32cubemx.license=1601130472038.product=STM32CubeMX.version=6.0.1.html
+	sudo unzip stmcube/*.zip
 	sudo chmod +x stmcube/SetupSTM32CubeMX-6.0.1.linux
 	./stmcube/SetupSTM32CubeMX-6.0.1.linux
 
