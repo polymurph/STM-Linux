654db4b33c59b42162e2dc5f5275e05115e2f8d1
key <key@devbian>
2020-10-02 16:18:41 +0200

    Brutal Change of gcc to g++

--- stm32g474_blinky/Core/Src/main.c
+++ stm32g474_blinky/Core/Src/main.c
@@ -99,7 +99,7 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 	
-	app_main();
+	//app_main();
 	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
 	HAL_Delay(300);
 	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
